48fb4bb13ad6aa2490d670e81e2a5adc2591cc3b
Mathias Teugels <mathias@codercpf.be>
2024-04-29 17:58:55 +0200

    Add ISO keyboard to vial
    UNTESTED

---
+++ keyboards/framework/iso/keymaps/vial/config.h
@@ -0,0 +1,7 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+#define VIAL_KEYBOARD_UID {0x81, 0x21, 0xF5, 0x4D, 0x42, 0xF4, 0x25, 0xFF}
+
+#define VIAL_UNLOCK_COMBO_ROWS { 2, 1 }
+#define VIAL_UNLOCK_COMBO_COLS { 2, 4 }
---
+++ keyboards/framework/iso/keymaps/vial/rules.mk
@@ -0,0 +1,6 @@
+VIA_ENABLE  = yes
+VIAL_ENABLE = yes
+
+# Not sure if necessary
+BACKLIGHT_ENABLE = yes
+BACKLIGHT_DRIVER = pwm
